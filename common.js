4cfe4dbf09d398182ef3260bca6d4de1052dee34
nect2r <nect2r@kakao.com>
2022-10-09 23:41:37 +0900

    is function 문법정리

--- common.js
+++ common.js
@@ -8,8 +8,10 @@
  * 
  * -- 함수명 정의 --
  * -- ※ 최대한 정의에 맞게 작성하려 했으나 isEmpty처럼 해당되지 않을 수 있으니 사용 전 함수 확인 필 ※
- * -- is*() {} 오직 *만 가지고 있는지 확인한다.
- * -- has*() {} *를 가지고 있는지 확인한다.
+ * -- is* () {} * 에 해당하는지 확인한다.
+ * -- is*With* () {} n 에 모두 해당하는지 확인한다.
+ * -- is*by<Text> () {} <Text>에 의해 * 에 해당하는지 확인한다.
+ * -- has* () {} *이 포함되는지 확인한다.
  */
 
 /**
@@ -115,10 +117,10 @@ function isEmpty(value) {
 }
 
 /**
- * 파라미터가 오직 한글인지 확인한다.(자음,모음도 포함)
+ * 파라미터가 한글인지 확인한다.(자음,모음도 포함)
  * 
  * @param {*} value 
- * @returns 한글만 있으면 true, isEmpty() 또는 아니면 false
+ * @returns 한글만 있으면 true, isEmpty() 아니면 false
  */
  function isKorean(value) {
     var reg = /^[ㄱ-ㅎ가-힣-ㅏ-ㅣ]+$/
@@ -133,10 +135,10 @@ function isEmpty(value) {
 }
 
 /**
- * 파라미터가 오직 알파벳인지 확인한다.
+ * 파라미터가 알파벳인지 확인한다.
  * 
  * @param {*} value 
- * @returns 알파벳만 있으면 true, isEmpty() 또는 아니면 false
+ * @returns 알파벳만 있으면 true, isEmpty() 아니면 false
  */
 function isAlphabet(value) {
     var reg = /^[a-zA-Z]+$/
@@ -151,13 +153,46 @@ function isAlphabet(value) {
 }
 
 /**
- * 파라미터가 정수인지 여부
+ * 파라미터가 정수인지 확인한다.(타입 미고려)
  * 
  * @param {*} value 
- * @returns 숫자만 있으면 true, isEmpty() 또는 아니면 false
+ * @returns 정수면 true, isEmpty() 아니면 false
  */
  function isInteger(value) {
+    if (isEmpty(value)) return false
+
+    if (checkType(value) == 'object') {
+        return Number(value.val()) % 1 === 0
+    } else {
+        return Number(value) % 1 === 0
+    }
+}
+
+/**
+ * 파라미터가 정수인지 확인한다.(정규식)
+ * 
+ * @param {*} value 
+ * @returns 정수면 true, isEmpty() 아니면 false
+ */
+ function isIntegerByRegExp(value) {
     var reg = /^[0-9]+$/
+
+    if (isEmpty(value)) return false
+
+    if (checkType(value) == 'object') {
+        return reg.test(value.val())
+    } else {
+        return reg.test(value)
+    }
+}
+/**
+ * 파라미터가 소수인지 확인한다.(정규식)
+ * 
+ * @param {*} value 
+ * @returns 소수면 true, isEmpty() 아니면 false
+ */
+function isFloatByRegExp(value) {
+    var reg = /^\d+?[.]\d+?$/
 
     if (isEmpty(value)) return false
 
@@ -169,13 +204,13 @@ function isAlphabet(value) {
 }
 
 /**
- * 파라미터가 오직 숫자인지 여부
+ * 파라미터가 숫자인지 확인한다.(정수,실수)
  * 
  * @param {*} value 
- * @returns 숫자만 있으면 true, isEmpty() 또는 아니면 false
+ * @returns 숫자면 true, isEmpty() 아니면 false
  */
 function isNumber(value) {
-    var reg = /^[0-9]+$/
+    var reg = /^(([\d])|(\d+?[.]{1}\d+?))+$/
 
     if (isEmpty(value)) return false
 
@@ -187,13 +222,13 @@ function isNumber(value) {
 }
 
 /**
- * 파라미터가 숫자와영문인지 여부
+ * 파라미터가 알파벳과 정수만 있는지 확인한다.
  * 
  * @param {*} value 
- * @returns 숫자와알파벳만 true, 아니면 false
+ * @returns 알파벳과 정수만 true, 아니면 false
  */
-function isAlphabetWithDigit(value) {
-    var reg = /[^a-zA-Z0-9]/
+function isAlphabetWithInteger(value) {
+    var reg = /^[a-zA-Z0-9]+$/
 
     if (isEmpty(value)) return false
 
@@ -205,7 +240,7 @@ function isAlphabetWithDigit(value) {
 }
 
 /**
- * 파라미터가 사업자번호인지 여부
+ * 파라미터가 사업자번호인지 확인한다.
  * 
  * @param {*} value 
  * @returns 사업자번호면 true, 아니면 false
@@ -216,32 +251,76 @@ function isBranchRegnum(value) {
     if (isEmpty(value)) return false
 
     if (checkType(value) == 'object') {
-        return !reg.test(value.val())
+        return reg.test(value.val())
     } else {
-        return !reg.test(value)
-    }
-}
-
-/**
- * 파라미터에 공백존재 여부
- * 
- * @param {*} value 
- * @returns 공백이 존재하면 true, 아니면 false
- */
-function isSpace(value) {
-    var reg = / /
-
-    if (isEmpty(value)) return false
-
-    if (checkType(value) == 'object') {
-        return !reg.test(value.val())
-    } else {
-        return !reg.test(value)
+        return reg.test(value)
     }
 }
 
 /**
  * End of is function
+ */
+
+/**
+ * Start of has function
+ */
+
+/**
+ * 파라미터가 한글을 포함하는지 확인한다.(자음,모음도 포함)
+ * 
+ * @param {*} value 
+ * @returns 한글을 포함하면 true, isEmpty() 아니면 false
+ */
+ function hasKorean(value) {
+    var reg = /[ㄱ-ㅎ가-힣-ㅏ-ㅣ]/
+
+    if (isEmpty(value)) return false
+
+    if (checkType(value) == 'object') {
+        return reg.test(value.val())
+    } else {
+        return reg.test(value)
+    }
+}
+
+/**
+ * 파라미터가 알파벳을 포함하는지 확인한다.
+ * 
+ * @param {*} value 
+ * @returns 포함하면 true, isEmpty() 아니면 false
+ */
+ function hasAlphabet(value) {
+    var reg = /[a-zA-Z]/
+
+    if (isEmpty(value)) return false
+
+    if (checkType(value) == 'object') {
+        return reg.test(value.val())
+    } else {
+        return reg.test(value)
+    }
+}
+
+/**
+ * 파라미터에 공백이 포함되는지 확인한다.
+ * 
+ * @param {*} value 
+ * @returns 공백이 존재하면 true, 아니면 false
+ */
+ function hasSpace(value) {
+    var reg = / /
+
+    if (isEmpty(value)) return false
+
+    if (checkType(value) == 'object') {
+        return reg.test(value.val())
+    } else {
+        return reg.test(value)
+    }
+}
+
+/**
+ * End of has function
  */
 
 /**
@@ -375,47 +454,3 @@ function getCurrentScreenId() {
 
     return currentUrl.substring(currentUrl.lastIndexOf('/') + 1, currentUrl.indexOf('.dev'));   // '/'문자 부터 문자열의 처음까지
 }
-
-/**
- * Start of has function
- */
-
-/**
- * 파라미터가 한글을 가지고 있는지 확인한다.(자음,모음도 포함)
- * 
- * @param {*} value 
- * @returns 한글이 있으면 true, isEmpty() 또는 아니면 false
- */
- function hasKorean(value) {
-    var reg = /[ㄱ-ㅎ가-힣-ㅏ-ㅣ]/
-
-    if (isEmpty(value)) return false
-
-    if (checkType(value) == 'object') {
-        return reg.test(value.val())
-    } else {
-        return reg.test(value)
-    }
-}
-
-/**
- * 파라미터가 알파벳을 가지고 있는지 확인한다.
- * 
- * @param {*} value 
- * @returns 알파벳이 있으면 true, isEmpty() 또는 아니면 false
- */
- function hasAlphabet(value) {
-    var reg = /[a-zA-Z]/
-
-    if (isEmpty(value)) return false
-
-    if (checkType(value) == 'object') {
-        return reg.test(value.val())
-    } else {
-        return reg.test(value)
-    }
-}
-
-/**
- * End of has function
- */
